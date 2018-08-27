94f2d4483ee1751a26a2e77eea52505320b9986b
Sarkhan Rasullu <sarkhanrasullu@gmail.com>
2018-08-28 01:27:41 +0400

    changes

--- Assets/SceneMaker.cs
+++ Assets/SceneMaker.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class SceneMaker : MonoBehaviour {
 
 	public List<GameObject> places;
+	public GameObject startPlace;
+	public GameObject finishPlace;
 	public GameObject character;
 
-	private System.Random rnd = new System.Random();
-	private GameObject lastPlace;
+
 
 	// Use this for initialization
 	void Start () {
+		startX = 0;
+		startY = 0;
 		Genereate ();
 	}
 	
@@ -25,37 +28,58 @@ public class SceneMaker : MonoBehaviour {
 		if (places == null || places.Count == 0)
 			return;
 		
-		int startX = 0;
-		int startY = 0;
-		int startZ = 0;
-
+		InstantiatePlace (startPlace);
 		for (int i = 0; i < 10; i++) {
 		    int index = rnd.Next(0, places.Count); 
 			GameObject obj = places [index];
 
-			if (lastPlace!=null) {
-				float lastPlaceWidth = Get_Width (lastPlace);
-				//int characterWidtdth = Get_Width (character);
-				startX += (int) lastPlaceWidth + 1;
-				Debug.Log ("last place width=" + lastPlaceWidth);
-				Debug.Log ("startX=" + startX);
-			}
-
-			obj.transform.rotation = transform.rotation;
-			obj.transform.position = new Vector3 (startX, startY, startZ);
-			Instantiate (obj);
-
-			lastPlace = obj;
+			InstantiatePlace (obj);
 		}
+		InstantiatePlace (finishPlace);
+	}
+
+	private  System.Random rnd = new System.Random();
+	private  GameObject lastPlace;
+	private  int startX;
+	private  int startY;
+
+	public  void InstantiatePlace(GameObject obj){
+		if (lastPlace!=null) {
+			int lastPlaceWidth =(int) Get_Width (lastPlace);
+			int lastPlaceHeight =(int) Get_Height (lastPlace);
+			//int characterWidtdth = Get_Width (character);
+			startX +=  lastPlaceWidth + rnd.Next (1, 4);
+			int upOrDown = rnd.Next (0, 3);
+			if (upOrDown == 0) {//up
+				startY += lastPlaceHeight;
+			} else if (upOrDown == 1) {//down
+				startY -= lastPlaceHeight + rnd.Next (2, 5);
+			}//stable
+			Debug.Log ("last place width=" + lastPlaceWidth);
+		}
+
+		obj.transform.rotation = transform.rotation;
+		obj.transform.position = new Vector3 (startX, startY, 0);
+		Instantiate (obj);
+
+		lastPlace = obj;
+		Debug.Log ("startX=" + startX);
+		Debug.Log ("obj.transform.position=" + obj.transform.position);
 	}
  
 	public static float Get_Width(GameObject gameObject)
 	{
-		float width = 0;
-
 		var collider2D= gameObject.GetComponent<BoxCollider2D>();
-		width = collider2D.size.x; //Edit, thanks to Arttu's comment!
+		float width = collider2D.size.x; //Edit, thanks to Arttu's comment!
 
 		return width;
+	}
+
+	public static float Get_Height(GameObject gameObject)
+	{
+		var collider2D= gameObject.GetComponent<BoxCollider2D>();
+		float height = collider2D.size.y; //Edit, thanks to Arttu's comment!
+
+		return height;
 	}
 }
